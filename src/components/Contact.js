6ea607ecffc0fbd838759cd2bc07feed4078077a
Colin Woodward <woodwardwebdev@gmail.com>
2020-09-09 19:31:06 +0100

    add CV file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,6 +33,7 @@ export default function Contact(props) {
       <Container className={isBig ? classes.formContainer : null}>
         <ContactForm />
       </Container>
+      <Container></Container>
     </div>
   );
 }
